3d02e0a37f58282114fbbc6e5e26879c5ee47c2c
mct <minsa97@naver.com>
2022-04-24 09:37:57 +0900

    0424:Initial commit

---
+++ src/main/java/com/mctlhg/ex03/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.mctlhg.ex03;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Ex03Application.class);
+	}
+
+}
