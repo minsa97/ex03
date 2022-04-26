3495d15ceda20140c3e227fbbd4897882eaeacc8
mct <minsa97@naver.com>
2022-04-26 09:26:58 +0900

    0426: Thymeleaf 레이아웃

--- src/main/java/com/mctlhg/ex03/controller/SampleController.java
+++ src/main/java/com/mctlhg/ex03/controller/SampleController.java
@@ -22,7 +22,7 @@ public class SampleController {
         log.info("ex1............");
     }
 
-    @GetMapping("/ex2")
+    @GetMapping({"/ex2", "/exLink"})
     public void exModel(Model model){
         List<SampleDTO> list= IntStream.rangeClosed(1,20).asLongStream().mapToObj(i->{
             SampleDTO dto=SampleDTO.builder()
@@ -58,4 +58,10 @@ public class SampleController {
     public void ex3(){
         log.info("ex3");
     }
+
+    @GetMapping({"/exLayout1", "/exLayout2", "/exTemplate", "exSidebar"})
+    public void exLayout1(){
+        log.info("exLayout ...............");
+    }
+
 }
